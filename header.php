d7c89c615a67c619640c95031f4b2462973b1245
shlepi88 <nemanjalojpur88@gmail.com>
2019-05-26 08:21:24 +0200

    update

--- header.php
+++ header.php
@@ -25,8 +25,7 @@
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
       <a class="navbar-brand" href="<?php echo home_url(); ?>">
-        <?php the_custom_logo(); ?>
-        <p><?php bloginfo('name'); ?></p>
+        <?php bloginfo('name'); ?>
       </a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
